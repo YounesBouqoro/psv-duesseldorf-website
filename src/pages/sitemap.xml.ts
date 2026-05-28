fde72e9d8badd7ead28d244bee2b3beac59a147b
Younes <younes.bouqoro@zuitable.com>
2026-05-28 13:07:01 +0200

    Add robots and sitemap

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,44 @@
+﻿import { getCollection } from "astro:content";
+
+export async function GET() {
+  const siteUrl = "https://younesbouqoro.github.io/psv-duesseldorf-website";
+
+  const staticPages = [
+    "",
+    "verein",
+    "sportarten",
+    "probetraining",
+    "mitglied-werden",
+    "downloads",
+    "partner",
+    "sponsoring",
+    "kinderschutz",
+    "kontakt",
+    "impressum",
+    "datenschutz"
+  ];
+
+  const sportarten = await getCollection("sportarten");
+
+  const urls = [
+    ...staticPages.map((page) => `${siteUrl}/${page}`.replace(/\/$/, "")),
+    ...sportarten.map((sport) => `${siteUrl}/sportarten/${sport.slug}`)
+  ];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls
+  .map(
+    (url) => `  <url>
+    <loc>${url}</loc>
+  </url>`
+  )
+  .join("\n")}
+</urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      "Content-Type": "application/xml"
+    }
+  });
+}
